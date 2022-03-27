20e2b8ec3f65e57e1fce81097c0f5fac825cd3cb
Ian Cargill <idcargill@gmail.com>
2022-03-26 17:58:05 -0700

    wip

--- project/settings.py
+++ project/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
